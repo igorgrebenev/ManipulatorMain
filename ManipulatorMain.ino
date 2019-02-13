dd43eb155cdd04c29b6f9b6c7dd8a945a8a3ffbe
igor.grebenev@quantumsoft.ru <igor.grebenev@quantumsoft.ru>
2019-02-13 07:59:45 +0700

    main

--- ManipulatorMain.ino
+++ ManipulatorMain.ino
@@ -1,18 +1,18 @@
-// Maksim Grebenev 2018
+ // Maksim Grebenev 2018
 #import "SGServo.h"
 #import "ROT3U6DOF.h"
 
 void setup() {
   setupROT3U6DOF(0, 0); // 500-900Initialize servos ROT3U6DOF_SERVO_COUNT/ROT3U6DOF_SERVO_START_PORT
   resetROT3U6DOF(); // установим кутяплю в исходное положение
-  delay(2000); // подождем 2 сек
+  delay(6000); // подождем 1 мин для прогрева потенцииометра
 }
 
 void loop() {
-  int positions00[] = {28, 65, 120, 150, 45, 20};
+  int positions00[] = {27, 65, 120, 180, 15, 20};
   performAllServos(positions00);
 
-  int positions01[] = {UNDEFINED, 62, UNDEFINED, 131, UNDEFINED, UNDEFINED};
+  int positions01[] = {UNDEFINED, 64, UNDEFINED, 131, UNDEFINED, UNDEFINED};
   performAllServos(positions01);
   
   int positions02[] = {UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, 70};
@@ -21,13 +21,18 @@ void loop() {
   int positions03[] = {UNDEFINED, 90, 45, UNDEFINED, UNDEFINED, UNDEFINED};
   performAllServos(positions03);  
 
-  int positions04[] = {140, 0, 97, 0, 0, UNDEFINED};
+  int positions04[] = {142, 0, 102, 0, 0, UNDEFINED};
   performAllServos(positions04);
-
+  
+  // бросить шарик
   int positions05[] = {UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, 20};
   performAllServos(positions05);
 
-  int positions06[] = {UNDEFINED, 90, 45, 45, UNDEFINED, UNDEFINED};
+  // подготовка у подъему манипулятора
+  int positions06a[] = {UNDEFINED, UNDEFINED, 0, 0, UNDEFINED, UNDEFINED};
+  performAllServos(positions06a);
+
+  int positions06[] = {UNDEFINED, 90, 45, 20, UNDEFINED, UNDEFINED};
   performAllServos(positions06);
 
   delay(3000); 
